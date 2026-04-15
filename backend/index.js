3b8fb49845345b663bb2f694c30cd9ee27a013e6
Sebastian Rodrigo Arce Bracamonte <sa2019062986@virtual.upt.pe>
2026-04-15 00:08:47 -0500

    fix socket.io cors policy

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: '*', // Permitir conexión desde cualquier React frontend
     methods: ['GET', 'POST']
   }
 });
